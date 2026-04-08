a8f595d2862334cdf3bb5c7fcaf139528ec21557
HBMG_Muffin <supermuffin0615@gmail.com>
2026-04-07 22:19:30 -0400

    adding queue and queue practice

---
+++ Queue/Practice1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Practice1
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Queue Practice 1");
+
+            Queue queue = new Queue();
+
+            Console.WriteLine("Input 10 Messages:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                string message = Console.ReadLine();
+                queue.Enqueue(message);
+            }
+
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item);
+                Thread.Sleep(1000);// Wait for 1 second before printing the next message
+            }
+           
+
+            
+        }
+    }
+}
--- Queue/Queue/Program.cs
+++ Queue/Queue/Program.cs
@@ -21,6 +21,58 @@ namespace Lesson3_Queue
             queue.Enqueue("Third");
             #endregion
 
+            #region Dequeue
+            //Dequeue is the operation of removing an item from the front of the queue. It returns the item that was removed.
+            object v = queue.Dequeue();
+            Console.WriteLine(v); // Output: First
+            #endregion
+
+
+            #region Peek
+            //Peek is the operation of viewing the item at the front of the queue without removing it. It returns the item at the front of the queue.
+            object v2 = queue.Peek();
+            Console.WriteLine(v2); // Output: Second
+
+        
+            if(queue.Contains("Third"))
+            {
+                Console.WriteLine("Queue contains 'Third'");
+            }
+
+            #endregion
+
+
+            #region clear
+            //Clear is the operation of removing all items from the queue.
+            //queue.Clear();
+             #endregion 
+
+            #region Count
+            //Count is a property that returns the number of items in the queue.
+            Console.WriteLine(queue.Count); 
+
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            object[] array = queue.ToArray();
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+
+            while(queue.Count > 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+
+            Console.WriteLine(queue.Count); 
+
+             #endregion
+
 
 
 
